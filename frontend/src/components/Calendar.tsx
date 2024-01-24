fb4d7e2362d5aef3701e89cb4d317639f22557f6
durotolu <durolawk@gmail.com>
2024-01-24 21:23:05 +0100

    calendar page cleanup

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,13 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-import dynamic from "next/dynamic";
-import { Logo } from "./Icons/Logo";
-import { AppointmentIcon } from "./Icons/AppointmentIcon";
-import { DashboardIcon } from "./DashboardIcon";
-import { Bell } from "./Icons/Bell";
-import { LeftArrow } from "./Icons/LeftArrow";
 import Scheduler from "./scheduler";
+import Sidebar from "./Sidebar";
 
 export interface Appointment {
   StartTime: string;
@@ -19,7 +14,6 @@ export interface Appointment {
 const Calendar = () => {
   const apiUrl: string =
     process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
-
   const [appointments, setAppointments] = useState<Appointment[]>([]);
 
   useEffect(() => {
@@ -55,41 +49,9 @@ const Calendar = () => {
   }, [apiUrl]);
 
   return (
-    <div className="h-full flex">
-      <div className="bg-white shadow">
-        <div className="flex-col justify-start items-start gap-2 inline-flex h-screen">
-          <div className="self-stretch pl-10 pr-6 py-12 justify-between items-center inline-flex">
-            <div className="justify-start items-center gap-2 flex">
-              <div className="text-stone-800 text-base font-medium font-['General Sans'] leading-relaxed">
-                <Logo />
-              </div>
-            </div>
-            <div className="w-2 h-2 bg-white rounded-full" />
-          </div>
-          <div className="self-stretch pl-10 pr-6 py-4 justify-between items-center inline-flex">
-            <div className="justify-start items-center gap-2 flex">
-              <DashboardIcon />
-              <div className="text-stone-800 text-base font-medium font-['General Sans'] leading-relaxed">
-                Dashboard
-              </div>
-            </div>
-            <div className="w-2 h-2 bg-white rounded-full" />
-          </div>
-          <div className="self-stretch pl-10 pr-6 py-4 bg-emerald-50 justify-between items-center inline-flex">
-            <div className="justify-start items-center gap-2 flex mr-16">
-              <AppointmentIcon />
-              <div className="text-green-700 text-base font-semibold font-['General Sans'] leading-relaxed">
-                Appointments
-              </div>
-            </div>
-            <div className="w-2 h-2 bg-green-700 rounded-full" />
-          </div>
-        </div>
-        <div className="w-[156px] h-[38.10px] left-[43px] top-[50.81px] absolute"></div>
-      </div>
-      <div>
-        <Scheduler events={appointments} />
-      </div>
+    <div className="h-full flex relative">
+      <Sidebar />
+      <Scheduler events={appointments} />
     </div>
   );
 };
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import { DashboardIcon } from "./DashboardIcon";
 
 const Sidebar = () => {
   return (
-    <div className="shadow">
+    <div className="shadow bg-white relative">
       <div className="flex-col justify-start items-start gap-2 inline-flex h-screen">
         <div className="self-stretch pl-10 pr-6 py-12 justify-between items-center inline-flex">
           <div className="justify-start items-center gap-2 flex">
