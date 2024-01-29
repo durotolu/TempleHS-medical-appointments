52f875117f651f53f2c1fc31629e2c89598cdb74
durotolu <durolawk@gmail.com>
2024-01-29 23:20:11 +0100

    update

--- backend/api/server.js
+++ backend/api/server.js
@@ -14,6 +14,13 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/doctors", doctorsRouter);
