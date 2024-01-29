566055fb5558e8e1d4f45bbc301f670d88c15f94
durotolu <durolawk@gmail.com>
2024-01-29 23:40:14 +0100

    update

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require('cors');
+const cors = require('cors');
 // const helmet = require('helmet');
 
 const authRouter = require("./auth/auth-router");
@@ -12,14 +12,20 @@ const app = express();
 // app.use(helmet());
 // app.use(cors());
 
+app.use(cors({
+  origin: process.env.NEXT_PUBLIC_API_URL
+}));
+
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.NEXT_PUBLIC_API_URL);
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// console.log(process.env.NEXT_PUBLIC_API_URL)
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", process.env.NEXT_PUBLIC_API_URL);
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.sendStatus(204);
+//   next();
+// });
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
