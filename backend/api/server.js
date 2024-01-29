5023967288e4eb1c1845bd4b043fec894aa19104
durotolu <durolawk@gmail.com>
2024-01-29 23:28:04 +0100

    update

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require('cors');
-const helmet = require('helmet');
+// const cors = require('cors');
+// const helmet = require('helmet');
 
 const authRouter = require("./auth/auth-router");
 const usersRouter = require("./users/user-router");
@@ -9,14 +9,14 @@ const appointmentsRouter = require("./appointments/appointments-router");
 
 const app = express();
 
-app.use(helmet());
-app.use(cors());
+// app.use(helmet());
+// app.use(cors());
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Origin", process.env.NEXT_PUBLIC_API_URL);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
