974b8a2a6c49e566dfd0425360e3a18297a8467a
durotolu <durolawk@gmail.com>
2024-01-31 23:52:05 +0100

    configured env variables

--- backend/api/server.js
+++ backend/api/server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", process.env.NODE_ALLOW);
+  // res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,10 +5,17 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DATABASE,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      uri: process.env.POSTGRES_URL,
+      ssl: true,
+
+      // host: process.env.DB_HOST,
+      // database: process.env.DB_NAME,
+      // user: process.env.DB_USER,
+      // password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
